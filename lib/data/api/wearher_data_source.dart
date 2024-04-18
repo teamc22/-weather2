eefa202114c30a611f7361dfd9eae8286c5e8bfe
jin song <songjin3914@gmail.com>
2024-04-18 15:18:45 +0900

    api create

---
+++ lib/data/api/wearher_data_source.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+
+import '../dto/weather_dto.dart';
+
+class PhotoDataSource {
+  final _baseUrl = 'https://pixabay.com/api/';
+
+  Future<WeatherDto> getWearthResult(String query) async {
+    final response = await http.get(Uri.parse(
+        '$_baseUrl?key=https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl&latitude=37.5&longitude=126.9'));
+
+    return WeatherDto.fromJson(jsonDecode(response.body));
+  }
+}
