c217d389505d725e5b512d8c7d8431077058616e
icicle_4 <dwayne.han4@gmail.com>
2024-04-18 15:23:52 +0900

    feat: test2

--- lib/data/api/wearher_data_source.dart
+++ lib/data/api/wearher_data_source.dart
@@ -11,6 +11,7 @@ class PhotoDataSource {
     final response = await http
         .get(Uri.parse('$_baseUrl?key=https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,weathercode,relativehumidity_2m,windspeed_10m,pressure_msl&latitude=37.5&longitude=126.9'));
     print('4월18일');
+    print('목요일');
     return WeatherDto.fromJson(jsonDecode(response.body));
   }
 }
